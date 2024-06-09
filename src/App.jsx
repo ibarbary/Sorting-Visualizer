0614b570a7609094960ed98fddabd1d26855eabe
ahmed elbarbary <aelbarbary120@gmail.com>
2024-06-09 15:24:32 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import BarsContainer from './components/BarsContainer';
+import Navbar from "./components/Navbar"
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar/>
+      <BarsContainer/>
+    </>
+  );
+}
+
+export default App
